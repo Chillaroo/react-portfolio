e77c7eff4825dbd6930c032aedb67f67890a8925
Rachel <rtc145@gmail.com>
2021-10-22 21:20:05 -0400

    set up react components and dependencies

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>
+        I've been building web applications and learning software development for nearly six months, and I can't wait to apply these skills in a new career! I have a background in mathematics and physics, and I've taught or tutored these subjects for the past ten years. I quickly adapt to rapidly changing job requirements, and I love working on teams to problem-solve.
+      </p>
+    </div>
+  );
+}
